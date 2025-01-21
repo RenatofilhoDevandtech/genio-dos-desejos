112650a35b78a1efb9d85a2da133be9f1f1e7200
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2025-01-21 12:13:13 -0300

    fix(�styles): �️ corrige o caminho incorreto para o arquivo app.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import '../src/Assets/Styles/app.scss';
+import '../src/assets/Styles/app.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
